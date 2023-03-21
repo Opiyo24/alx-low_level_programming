bf42dad10d92658adec270b499f3744b8081c357
Opiyo24 <gabbynyangor@gmail.com>
2023-03-21 13:19:52 -0700

    Correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - prints 10 times the alphabet in lowercase followed by a new line
- *
- * Return: Always 0
+ * 2-print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * followed by a new line
 */
 
 void print_alphabet_x10(void)
