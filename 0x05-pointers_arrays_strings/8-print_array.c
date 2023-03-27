4b5d108fe5c013a129c09909108dc5272d6b0301
Opiyo24 <gabbynyangor@gmail.com>
2023-03-27 11:16:47 -0700

    n elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * input_array - prints n elements of an array
+ * _putchar - prototype function
+ * @a: array name
+ * @n: number of elements of the array to be printed
+ * Return: a and n inputs
+*/
+
+void input_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d,", a[i]);
+	}
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	printf("\n");
+}
