775288e9c70f705692bb528531e78252b9d690b3
Opiyo24 <gabbynyangor@gmail.com>
2023-04-17 12:40:32 -0700

    Love

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a new data structure named dog
+ * @*d: The new structure name
+ * 
+ * Return: A s directed
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+
+}
