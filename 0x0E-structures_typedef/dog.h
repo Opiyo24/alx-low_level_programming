1365f2c89d90fdaa2309db1dc303fb2b4e2b319b
Opiyo24 <gabbynyangor@gmail.com>
2023-04-17 12:10:46 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - new structure declaration
+ * @name: structure element 1
+ * @age: second structure element
+ * @owner: structure element 3
+ *
+ * Description: Additional structure description
+*/
+
 struct dog
 {
 	char *name;
