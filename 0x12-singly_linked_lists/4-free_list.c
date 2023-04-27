52ca94ff0661b3c820d9caf303c54306ab7c1aeb
Opiyo24 <gabbynyangor@gmail.com>
2023-04-27 04:59:13 -0700

    Correction

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,19 +2,17 @@
 #include "lists.h"
 
 /**
-* free_list – frees a linked list
+* free_list –releases/ frees the memory previously allocated to a linked list
 * @head: list_t list to be freed
 * Return: void
 */
 void free_list(list_t *head)
 {
-	list_t *temp;
-
-	while (head)
+	if (head)
 	{
-		temp = head->next;
-		free(head->str);
+		free_list(head->next);
+		if(head->str)
+			free(head->str);
 		free(head);
-	head = temp;
 	}
 }
